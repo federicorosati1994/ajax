a9747128c08eebdf8ae67c93b235cfb27287627a
federicorosati1994 <federicorosati1994@hotmail.it>
2016-03-07 17:31:17 +0100

    Exercise 2

---
+++ ajax_ex2/js/myjs.js
@@ -0,0 +1,56 @@
+var numberClick = 0;
+scrolled = true;
+
+var scrollMove;
+
+$(document).ready(function () {
+
+	var root = 'http://jsonplaceholder.typicode.com';
+
+	$(".orange").on("click",function(){
+
+		numberClick++;
+
+		$.ajax({
+			url: root + '/albums',
+			method: 'GET',
+			contentType: 'application/json',
+			dataType: 'json',
+			success: function(result){
+		        alert(result.length);
+		    },
+		    error: function (request, errorType, errorMessage){
+				alert('Error: ' + errorType + ', message: ' + errorMessage)
+				console.log(request);
+			}
+		})
+	})
+
+});
+
+$(".red").on("click",function(){
+	alert( numberClick )
+});
+
+$("#scroll_container").on("scroll", function() {
+	if(scrolled){
+		alert("Stai scrollando!");
+		scrolled=false;
+	};
+	if(scrollMove > $(this).scrollTop()){
+		console.log("stai salendo...")
+	}
+	if(scrollMove < $(this).scrollTop()){
+		console.log("stai scendendo...")
+	}
+	scrollMove = $(this).scrollTop();
+});
+
+$('#scroll_container').bind('scroll', function(){
+    if($(this).scrollTop() + $(this).innerHeight()>=$(this)[0].scrollHeight){
+        alert("Fine!");
+    };
+    if($(this).scrollTop() == 0){
+        alert("Inizio!");
+    };
+})
